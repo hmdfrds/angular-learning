77ad554d6e9f1f27050997abdf180601399dda0a
Firdaus <firdaus1337@gmail.com>
2025-07-06 19:52:50 +0800

    progress

--- learning-resources/src/app/learning-resources/learning-resources.component.ts
+++ learning-resources/src/app/learning-resources/learning-resources.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SafeLinkDirective } from '../safe-link.directive';
+import { LogDirective } from '../log.directive';
 
 @Component({
   selector: 'app-learning-resources',
--- learning-resources/src/app/safe-link.directive.ts
+++ learning-resources/src/app/safe-link.directive.ts
@@ -1,14 +1,19 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, input } from '@angular/core';
+import { LogDirective } from './log.directive';
 
 @Directive({
   selector: 'a[appSafeLink]',
   standalone: true,
   host: {
     '(click)': 'onConfirmLeavePage($event)'
-  }
+  },
+  hostDirectives: [LogDirective]
 })
 
 export class SafeLinkDirective {
+  queryParam = input("myapp", { alias: 'appSafeLink' });
+  private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
   constructor() {
     console.log("SafeLinkDirective is active!");
   }
@@ -17,8 +22,11 @@ export class SafeLinkDirective {
     const wantsToLeave = window.confirm("Do you want to leave this app?");
 
     if (wantsToLeave) {
+      const address = this.hostElementRef.nativeElement.href;
+      this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
       return;
     }
+
     event?.preventDefault()
   }
 
